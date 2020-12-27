a5f45279cbae253c95414f87e16ce8e7758e6a23
Satishc <satishchilkaka8@gmail.com>
2020-12-27 15:34:12 -0500

    Update nav.spec.js

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -2,10 +2,22 @@
 import login from './login.js'
 const userName = Cypress.env("userName");
 const pin = Cypress.env("pin")
+const rUser = Cypress.env("rUser");
+const amount = Cypress.env("amount")
+
 
 
 describe('Navigate to basic bank',() => {
     it('Should open basic bank', () => {
         login.loginUser(userName, pin)
+    })
+    it('send money to other user', () => {
+        login.loginUser(userName, pin)
+        cy.get('.form--transfer').within(() => {
+            cy.get('input:first').type(rUser, { delay: 0, force: true });
+            cy.get('input:last').type(amount, { delay: 0, force: true, log: false });
+            cy.get('.form__btn--transfer').click()
+        })
+
     })
 })
